6c02ebb50217177dc12239fb39bb1ca74ba8c0e4
Edouard Castaignet <ecastaignet@gmail.com>
2017-03-20 16:57:54 +0100

    validates ajoutés

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,22 @@
 class Customer < ApplicationRecord
+
+  # validates :customer_company_name, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :position_in_company, presence: true
+  # validates :mobile_phone_number, presence: true
+  # validates :office_phone_number, presence: true
+  # validates :email, presence: true
+  # validates :customer_address, presence: true
+  # validates :works_address, presence: true
+  # validates :needs_surface, presence: true
+  # validates :nb_of_employees, presence: true
+  # validates :budget, presence: true
+  # validates :starts_at, presence: true
+  # validates :ends_at, presence: true
+  # validates :customer_startup_style, presence: true
+  # validates :customer_classical_style, presence: true
+  # validates :customer_modern_style, presence: true
+  # validates :description, presence: true
+
 end
