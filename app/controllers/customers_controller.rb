ea01e99cafe512dc0aba21fadbb3d98943b4a882
Guillaume Salin <gsalin@gmail.com>
2017-03-21 16:29:44 +0100

    formulaire good, manque localisation google et toute la partie js ajax

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,21 @@
 class CustomersController < ApplicationController
   def new
+    @customer = Customer.new
   end
 
   def create
+    @customer = Customer.new(customer_params)
+      raise
+    if @customer.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:customer_company_name, :first_name, :last_name, :position_in_company, :mobile_phone_number, :office_phone_number, :email, :customer_address, :works_address, :needs_surface, :nb_of_employees, :budget, :starts_at, :ends_at, :customer_startup_style, :customer_classical_style, :customer_modern_style, :description)
   end
 end
