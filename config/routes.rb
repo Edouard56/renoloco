a112dcf8946fcab6cdb8c8035eaff277fed28144
Edouard Castaignet <ecastaignet@hotmail.com>
2017-03-24 15:00:47 +0100

    custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   # Edouard : j'ai overridé devise pour la redirection du sign up en créant un registrations controller
   devise_for :pros, controllers: { registrations: "registrations" }
 
-resources :customers, only: [:new, :create, :show]
+resources :customers, only: [ :create ]
 resources :pros, only: :show
 
+  get 'formulaire', to: 'customers#new'
+  get 'confirmation', to: 'customers#show'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
