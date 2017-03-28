dd7074ab7f7c0e08c268ac38f6c192c4b277c320
Guillaume Salin <gsalin@gmail.com>
2017-03-28 11:28:04 +0200

    debut active admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323114613) do
+ActiveRecord::Schema.define(version: 20170328091809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,18 +66,18 @@ ActiveRecord::Schema.define(version: 20170323114613) do
   end
 
   create_table "pros", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "pro_company_name"
     t.string   "address"
     t.float    "latitude"
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(version: 20170323114613) do
     t.boolean  "all_works"
     t.boolean  "architecte"
     t.integer  "max_operating_surface"
+    t.boolean  "admin",                  default: false, null: false
     t.index ["email"], name: "index_pros_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_pros_on_reset_password_token", unique: true, using: :btree
   end
