c104f1cdd0d02e97e97cf17deeb3e7a6e26e7c6d
Edouard Castaignet <ecastaignet@hotmail.com>
2017-03-29 11:31:26 +0200

    back working

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,31 @@ require 'faker'
 
 Match.destroy_all
 Pro.destroy_all
+Customer.destroy_all
+
+
+
+pro = Pro.new(
+    pro_company_name: "admin",
+    address: "1, rue de Maubeuge",
+    email: "ecastaignet@hotmail.com",
+    password: 123456,
+    latitude: 45.7589419,
+    longitude: 4.8309619,
+    contact_first_name: "Edouard",
+    contact_last_name: "Castaignet",
+    contact_position: "Admin",
+    contact_phone_number: "0112345678",
+    operating_radius: 50,
+    min_operating_surface: 100,
+    max_operating_surface: 500,
+    all_works: false,
+    architecte: false,
+    furnitures: false,
+    simulation: false,
+    admin: true
+  )
+  pro.save!
 
   pro = Pro.new(
     pro_company_name: Faker::Company.name,
@@ -19,9 +44,9 @@ Pro.destroy_all
     latitude: 45.7589419,
     longitude: 4.8309619,
     contact_first_name: "Edouard",
-    contact_last_name: Faker::Name,
+    contact_last_name: "Po",
     contact_position: "Président",
-    contact_phone_number: Faker::Number,
+    contact_phone_number: "0112345678",
     operating_radius: 50,
     min_operating_surface: 100,
     max_operating_surface: 500,
@@ -40,9 +65,9 @@ Pro.destroy_all
     latitude: 45.7589419,
     longitude: 4.8309619,
     contact_first_name: "Edouard",
-    contact_last_name: Faker::Name,
+    contact_last_name: "Jello",
     contact_position: "Président",
-    contact_phone_number: Faker::Number,
+    contact_phone_number: "0112345678",
     operating_radius: 50,
     min_operating_surface: 100,
     max_operating_surface: 500,
@@ -61,9 +86,9 @@ Pro.destroy_all
     latitude: 48.8697246,
     longitude: 2.3309619,
     contact_first_name: "Edouard",
-    contact_last_name: Faker::Name,
+    contact_last_name: "Minh",
     contact_position: "Président",
-    contact_phone_number: Faker::Number,
+    contact_phone_number: "0112345678",
     operating_radius: 50,
     min_operating_surface: 100,
     max_operating_surface: 500,
@@ -82,9 +107,9 @@ Pro.destroy_all
     latitude: 48.8697246,
     longitude: 2.3309619,
     contact_first_name: "Edouard",
-    contact_last_name: Faker::Name,
+    contact_last_name: "Mello",
     contact_position: "Président",
-    contact_phone_number: Faker::Number,
+    contact_phone_number: "0112345678",
     operating_radius: 50,
     min_operating_surface: 100,
     max_operating_surface: 500,
@@ -104,9 +129,9 @@ Pro.destroy_all
     latitude: 48.8697246,
     longitude: 2.3309619,
     contact_first_name: "Edouard",
-    contact_last_name: Faker::Name,
+    contact_last_name: "Mille",
     contact_position: "Président",
-    contact_phone_number: Faker::Number,
+    contact_phone_number: "0112345678",
     operating_radius: 50,
     min_operating_surface: 100,
     max_operating_surface: 500,
@@ -125,9 +150,9 @@ pro = Pro.new(
     latitude: 48.8697246,
     longitude: 2.3309619,
     contact_first_name: "Edouard",
-    contact_last_name: Faker::Name,
+    contact_last_name: "Best",
     contact_position: "Président",
-    contact_phone_number: Faker::Number,
+    contact_phone_number: "0112345678",
     operating_radius: 50,
     min_operating_surface: 100,
     max_operating_surface: 500,
@@ -147,9 +172,9 @@ pro = Pro.new(
     latitude: 48.8697246,
     longitude: 2.3309619,
     contact_first_name: "Edouard",
-    contact_last_name: Faker::Name,
+    contact_last_name: "Surimi",
     contact_position: "Président",
-    contact_phone_number: Faker::Number,
+    contact_phone_number: "0112345678",
     operating_radius: 50,
     min_operating_surface: 100,
     max_operating_surface: 500,
@@ -169,9 +194,9 @@ pro = Pro.new(
     latitude: 48.8697246,
     longitude: 2.3309619,
     contact_first_name: "Edouard",
-    contact_last_name: Faker::Name,
+    contact_last_name: "Faure",
     contact_position: "Président",
-    contact_phone_number: Faker::Number,
+    contact_phone_number: "0112345678",
     operating_radius: 50,
     min_operating_surface: 100,
     max_operating_surface: 500,
@@ -181,4 +206,46 @@ pro = Pro.new(
     simulation: false
   )
   pro.save!
+
+  pro = Pro.new(
+    pro_company_name: Faker::Company.name,
+    address: "Paris",
+    email: "paris7@mail.com",
+    password: 123456,
+    latitude: 48.8697246,
+    longitude: 2.3309619,
+    contact_first_name: "Edouard",
+    contact_last_name: "Faure",
+    contact_position: "Président",
+    contact_phone_number: "0112345678",
+    operating_radius: 50,
+    min_operating_surface: 500,
+    max_operating_surface: 1000,
+    all_works: false,
+    architecte: false,
+    furnitures: false,
+    simulation: false
+  )
+  pro.save!
+
+customer = Customer.new(
+  customer_company_name: Faker::Company.name,
+  first_name: "Jean-Pierre",
+  last_name: "Dick",
+  mobile_phone_number: "0712345678",
+  email: "customer@mail.com",
+  customer_address: "Versailles",
+  works_address: "Sèvres",
+  needs_surface: 200,
+  nb_of_employees: 25,
+  starts_at: "Thu, 30 Mar 2017 00:00:00 UTC +00:00",
+  ends_at: "Thu, 30 Mar 2017 00:00:00 UTC +00:00",
+  customer_startup_style: true,
+  description: "petite description",
+  latitude: 48.8167,
+  longitude: 2.2
+  )
+customer.save!
+
+
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
